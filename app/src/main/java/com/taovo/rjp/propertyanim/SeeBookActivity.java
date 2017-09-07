b128851cc2461059471e658296d3fae57a27e8f3
rjpacket <jimbo922@163.com>
2017-09-07 16:22:57 +0800

    电子书 翻页动画

--- app/src/main/java/com/taovo/rjp/propertyanim/SeeBookActivity.java
+++ app/src/main/java/com/taovo/rjp/propertyanim/SeeBookActivity.java
@@ -1,7 +1,13 @@
 package com.taovo.rjp.propertyanim;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.taovo.rjp.propertyanim.view.BookView;
 
 public class SeeBookActivity extends Activity {
 
@@ -9,5 +15,21 @@ public class SeeBookActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_book);
+
+        TextView tvPageOne = (TextView) findViewById(R.id.page_one);
+        TextView tvPageTwo = (TextView) findViewById(R.id.page_two);
+
+        BookView bookView = (BookView) findViewById(R.id.book_view);
+
+        bookView.setTextViews(tvPageOne, tvPageTwo);
+    }
+
+    public Bitmap getViewBitmap(View view) {
+        int width = view.getMeasuredWidth();
+        int height = view.getMeasuredHeight();
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
     }
 }
---
+++ app/src/main/java/com/taovo/rjp/propertyanim/view/README.md
@@ -0,0 +1,9 @@
+### 一、电子书翻页贝塞尔曲线实现
+前一篇说到电子书的打开效果，电子书的翻页效果更加绚丽。今天找到一个开源的[仿掌阅](https://github.com/Focfa/Jreader)项目，下载了源码看了一下，效果真的不错。
+代码也是来自于何大神的博客，如果不知道何大神的博客地址，我给个传送：
+[Android 实现书籍翻页效果----原理篇](http://blog.csdn.net/hmg25/article/details/6306479)
+[Android 实现书籍翻页效果----源码篇](http://blog.csdn.net/hmg25/article/details/6319664)
+[Android 实现书籍翻页效果----完结篇](http://blog.csdn.net/hmg25/article/details/6342539)
+[Android 实现书籍翻页效果---番外篇之光影效果](http://blog.csdn.net/hmg25/article/details/6366279)
+[Android 实现书籍翻页效果----升级篇](http://blog.csdn.net/hmg25/article/details/6419694)
+看完再把demo下载下来琢磨一下，基本怎么实现的也就会个7788了。
